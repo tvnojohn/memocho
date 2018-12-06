fdef31698143d57c5c7bbe9e3f65753efb3647b5
mj <detteiu.9.13@gmail.com>
2018-12-07 05:45:23 +0900

    複数のmemoを管理

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 $(function() {
-    if (localStorage.getItem('memo')) {
-        $('#memo').val(localStorage.getItem('memo'));
+    let memoNum = 0;
+    let data = [];
+    data = JSON.parse(localStorage['memo']);
+    for(var i=0; i<data.length; i++){
+        let a ='<ul class="collection" style="padding:0; margin:0;">'+ data[i].id +'</ul>';
+        $(a).appendTo('#memo-list');
     }
+    $('.textarea').val(data[memoNum].memo);
 
-    $('#clear').click(function() {
-        $('#memo').val('');
-        localStorage.removeItem('memo');
+    // $('#clear').click(function() {
+    //     $('.textarea').val('');
+    //     localStorage.removeItem('memo');
+    // });
+
+    $('.textarea').bind('keyup', function(){
+        data[memoNum].memo = $('.textarea').val();
+        localStorage.setItem('memo', JSON.stringify(data));
     });
+    
+    $(document).on('click','.collection', function () {
+        memoNum = this.innerHTML-1;
+        $('.textarea').val(data[memoNum].memo);
+        console.log($('.textarea'));
+    })
 
-    (function autoSave() {
-        localStorage.setItem('memo', $('#memo').val());
-        setTimeout(autoSave, 1000);
-    })();
+    $('.fixed-action-btn').click(function(){
+        const num = parseInt(data[data.length-1].id)+1
+        data.push({id:num.toString(), memo:''});
+        let a ='<ul class="collection" style="padding:0; margin:0;">'+ data[data.length-1].id +'</ul>';
+        $(a).appendTo('#memo-list');
+        memoNum = data.length-1;
+        $('.textarea').val(data[memoNum].memo);
+    })
 });
+
+function getLocalStorage () {
+    if (localStorage.getItem('memo')) {
+        return localStorage["memo"];;
+    } else {
+        return null;
+    }
+}
